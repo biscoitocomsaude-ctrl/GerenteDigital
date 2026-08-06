9597e30cdcf51e24ccba8121c1d2768d3eb53543
Quezia <biscoito.com.saude@gmail.com>
2026-08-06 20:14:29 -0300

    corrigindo erro config 05

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 const CONFIG = {
+
     NOME: "Gerente Digital",
+
     VERSAO: "2.0.0",
+
     API_URL: "https://script.google.com/macros/s/AKfycbxVsc6gzxbpkDYXVA7ZNxJheLmNjokqIJBgcaEGgfdDAl-SuEdkJL6uJgC4OjmiiSV9/exec",
+
     TOKEN: "gerentetorres"
+
 };
