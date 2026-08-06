3242ef4215fafd5dda6fd1824147242789f96b01
Quezia <biscoito.com.saude@gmail.com>
2026-08-06 19:46:17 -0300

    corrigindo erro do config 04

--- config.js
+++ config.js
@@ -1,21 +1,6 @@
-/**************************************************************
- * CONFIG.JS
- * Configurações do Gerente Digital
- **************************************************************/
 const CONFIG = {
-    //=========================================
-    // Sistema
-    //=========================================
-    NOME: "Gerente Digital API online",
+    NOME: "Gerente Digital",
     VERSAO: "2.0.0",
-    
-    //=========================================
-    // API Apps Script
-    //=========================================
     API_URL: "https://script.google.com/macros/s/AKfycbxVsc6gzxbpkDYXVA7ZNxJheLmNjokqIJBgcaEGgfdDAl-SuEdkJL6uJgC4OjmiiSV9/exec",
-    
-    //=========================================
-    // Segurança
-    //=========================================
     TOKEN: "gerentetorres"
 };
