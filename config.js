ceb5b189cb018481d81f9b6dbb86dd78c8859a8e
Quezia <biscoito.com.saude@gmail.com>
2026-08-06 21:50:39 -0300

    Atualizando a URL da API no arquivo de configuração

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     NOME: "Gerente Digital",
     VERSAO: "2.0.0",
-    API_URL: "https://script.google.com/macros/s/AKfycbxQNAshT0AcZszKu2pQICNmmlV1GJIRRnYuHRLWSKkeiGDaV_MVocY1PYa9EQc_mvs/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbxhEiPoAeV_TvCkskcgWT9VOR3M8vyk7jRhJPZVoaMQZ2lpC6HB1haIIr7i5UKbIpZk/exec",
     TOKEN: "gerentetorres"
 };
