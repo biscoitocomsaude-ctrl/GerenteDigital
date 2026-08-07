ffecb93242153627b81a82af30805bb7b32df53f
Quezia <biscoito.com.saude@gmail.com>
2026-08-06 21:06:35 -0300

    Corrigindo o erro config 08

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 const CONFIG = {
-    NOME: "Gerente Digital API online",
-    VERSÃO: "2.0.0",
-    API_URL: "https://script.google.com/macros/s/AKfycbxu-vnnCv1ELo_7rRAg8mIqgMpsWb7Ix8okpCVnvhLKm3DRMjpUqpDPD7BgUtCLeGwN/exec"
-
-    TOKEN: "gerentetorres"
-
+    NOME: "Gerente Digital",
+    VERSAO: "2.0.0",
+    API_URL: "https://script.google.com/macros/s/AKfycbxu-vnnCv1ELo_7rRAg8mIqgMpsWb7Ix8okpCVnvhLKm3DRMjpUqpDPD7BgUtCLeGwN/exec",
+   TOKEN: "gerentetorres"
 };
