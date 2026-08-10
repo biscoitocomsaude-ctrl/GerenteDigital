94550d5289fad2f133d5776fc492c0f81b5f7053
Quezia <biscoito.com.saude@gmail.com>
2026-08-10 20:29:00 -0300

    Atualizando a versão da API para 3.0.0 no arquivo de configuração

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     NOME: "Gerente Digital",
-    VERSAO: "2.0.0",
+    VERSAO: "3.0.0",
     API_URL: "https://script.google.com/macros/s/AKfycbwtxafPkqJ7itXFDYPEIm-ZTsGi0t25GDvsb4Mp2hnHN5zWEtdoQs_Gn1ckhStg82S8/exec",
     TOKEN: "gerentetorres"
 };
