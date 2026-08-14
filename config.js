a3fbf07d2955159797eb323a2f17cb6566fef133
Quezia <biscoito.com.saude@gmail.com>
2026-08-14 00:13:57 -0300

    Atualizando a versão do Gerente Digital para 2.2.0 nos arquivos app.js, config.js e style.css

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     NOME: "Gerente Digital",
-    VERSAO: "2.1",
-    API_URL: "https://script.google.com/macros/s/AKfycbzix8LxRBUqWBgjgV14a0NevVUjlN4e4SY3ww9xbkznQuV2CaPQuWqvT2qu-M7AmzAy/exec",
+    VERSAO: "2.2.0",
+    API_URL: "https://script.google.com/macros/s/AKfycbztOtAUSjCLklvAy51axBBpB0pYLBP-pHknw7oOxVTMq81asgd8S7fsVvqHuqHY4p6y/exec",
     TOKEN: "gerentetorres"
 };
