432af99a060a50e9683c8ef1a680a303958cc9ff
Quezia <biscoito.com.saude@gmail.com>
2026-08-14 01:03:39 -0300

    Corrigindo a versão do Gerente Digital para 2.2.0 no arquivo de configuração

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     NOME: "Gerente Digital",
-    VERSAO: "2.2.1",
+    VERSAO: "2.2.0",
     API_URL: "https://script.google.com/macros/s/AKfycbwB1_I-oEyOW-71ucSydPmhNsZzLTkcFXGQmS4eciA5rcJhbhW9VmnEbZI4Azn0sSCt/exec",
     TOKEN: "gerentetorres"
 };
